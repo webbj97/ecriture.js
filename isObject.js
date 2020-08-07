65cf14c3b3f1e862fb0e692248f33574982e4765
yuguang <yuguang@gaoding.com>
2020-08-07 14:53:46 +0800

    :art:删除无用代码，初始文件夹

--- isObject.js
+++ isObject.js
@@ -1,14 +1,10 @@
 /*
- * @desc: 判断是否是对象
+ * @desc:
  * @Author: 余光
  * @Email: webbj97@163.com
- * @Date: 2020-07-24 14:00:09
+ * @Date: 2020-07-28 10:46:06
  */
 /**
- * Checks if `value` is the
- * [language type](http://www.ecma-international.org/ecma-262/7.0/#sec-ecmascript-language-types)
- * of `Object`. (e.g. arrays, functions, objects, regexes, `new Number(0)`, and `new String('')`)
- *
  * 最终理解为，数组、函数、对象、正则、构造器生成的都归类为对象
  *
  * @param {*} value The value to check.
@@ -32,7 +28,6 @@ function isObject(value) {
     // if(value === null) return false; // null === null => true
     // if(type === 'object' || type === 'function') return true;
     // return false
-
     // 整理为下面的判断
 
     return value != null && (type === 'object' || type === 'function')
